10adb23c9fabfcfc75ea66d3b3ab91e45d7b5c47
Nickolas <35115545+NickolayBauer@users.noreply.github.com>
2018-04-09 10:02:20 +0300

    Update DataBase.py

--- DataBase.py
+++ DataBase.py
@@ -12,22 +12,37 @@ def show(table):
 
 
 def select_id(table, i):
-    return str(table[0] + '\n'+table[1] + '\n' + table[i - 1])
+    return str(table[0] + '\n' + table[1] + '\n' + table[i - 1])
 
 
 def select_were(table, sys, row):
     ls = []
-    for i in range(2,len(table)):
-        if table[i][table[1].index(sys)] == row:
+    for i in range(2, len(table)):
+        if row == '*':
+            ls.append(table[i])
+        elif table[i][table[1].index(sys)] == row:
             ls.append(table[i])
     return ls
+
+
+def sortd(table, param):
+    ls = []
+
+    for i in range(2, len(table)):
+        ls.append(table[i])
+    ls = sorted(ls, key=lambda row: row[table[1].index(param)])
+    ls.insert(0, table[0])
+    ls.insert(1, table[1])
+    return ls
+
 
 table = create('Персонажи')
 add(table, ['Имя', 'Игра'])
 add(table, ['Геральт', 'Ведьмак III'])
 add(table, ['Аджай', 'Far Cry IV'])
 add(table, ['Артём', 'Метро 2033'])
-add(table, ['Хантер','Метро 2033'])
-#show(table)
+add(table, ['Хантер', 'Метро 2033'])
+# show(table)
 
-print('this_is_select:',select_were(table, 'Игра', 'Метро 2033'))
+print('this_is_select:', select_were(table, 'Игра', 'Ведьмак III'))
+print(sortd(table, 'Имя'))
