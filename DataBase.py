6d74ae93740ab2325abee035b2173fe38f02c02a
Nickolas <35115545+NickolayBauer@users.noreply.github.com>
2018-04-10 00:55:51 +0300

    Update DataBase.py

--- DataBase.py
+++ DataBase.py
@@ -33,3 +33,13 @@ class Table:
     def sortd(self, param):
         return sorted(self.data, key=lambda row: row[self.system.index(param)])
 
+
+my_par = Table('Таблица один', *['Имя', 'Игра'])
+my_par.apnd(*['Артём', 'Метро'])
+my_par.apnd(*['Хантер', 'Метро'])
+my_par.apnd(*['Пейган', 'Far cry'])
+my_par.apnd(*['Геральт', 'Ведьмак'])
+my_par.show_me_all()
+print('_____________________________')
+print(my_par.select('Игра', 'Метро'))
+print(my_par.sortd('Имя'))
