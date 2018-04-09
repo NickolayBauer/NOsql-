aa0f0be1829714c6fba0d1f5036852f1177bd21c
Nickolas <35115545+NickolayBauer@users.noreply.github.com>
2018-04-10 00:55:35 +0300

    Переделал всё под ООП, наверное

--- DataBase.py
+++ DataBase.py
@@ -1,47 +1,35 @@
-def create(name):
-    return [name]
+class Table:
+    def __init__(self, name, *args):
+        self.name = name
+        data = []
+        self.data = data
+        system = []
+        for arg in args:
+            system.append(arg)
+        self.system = system
 
+    def apnd(self, *args):
 
-def add(table, *args):
-    table.append(*args)
+        if len(args) != len(self.system):
+            print('ашыпка')
+        else:
+            self.data.append(args)
 
+    def show_me_all(self):
 
-def show(table):
-    for i in table:
-        print(i)
+        print(self.name + '\n' + str(self.system))
+        for row in self.data:
+            print(row)
 
+    def select(self, chrome, xde):
+        ls = []
 
-def select_id(table, i):
-    return str(table[0] + '\n' + table[1] + '\n' + table[i - 1])
+        for row in self.data:
+            if row[self.system.index(chrome)] == xde:
+                ls.append(row)
 
+        return ls
 
-def select_were(table, sys, row):
-    ls = []
-    for i in range(2, len(table)):
-        if row == '*':
-            ls.append(table[i])
-        elif table[i][table[1].index(sys)] == row:
-            ls.append(table[i])
-    return ls
+    def sortd(self, param):
+        return sorted(self.data, key=lambda row: row[self.system.index(param)])
 
-
-def sortd(table, param):
-    ls = []
-    for i in range(2, len(table)):
-        ls.append(table[i])
-    ls = sorted(ls, key=lambda row: row[table[1].index(param)])
-    ls.insert(0, table[0])
-    ls.insert(1, table[1])
-    return ls
-
-
-table = create('Персонажи')
-add(table, ['Имя', 'Игра'])
-add(table, ['Геральт', 'Ведьмак III'])
-add(table, ['Аджай', 'Far Cry IV'])
-add(table, ['Артём', 'Метро 2033'])
-add(table, ['Хантер', 'Метро 2033'])
-# show(table)
-print(select_were(table,'Игра','Метро 2033'))
-print('this_is_select:', select_were(table, 'Игра', 'Ведьмак III'))
-print(sortd(table, 'Имя'))
